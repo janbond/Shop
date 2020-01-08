7ed41220ee6187c23fba408ffaf9907c55e982e3
janbond <59654523+janbond@users.noreply.github.com>
2020-01-08 23:02:22 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,43 @@
+﻿jQuery(document).ready(function(e) {
+	
+	/* DIV高度自動延展100% (載入、平板翻轉、視窗縮放) */
+  var $win = jQuery(window), $body = jQuery('body');
+  $win.bind('load resize', function(e) {
+    var $autoDiv = jQuery('.autoOn');
+    if ($autoDiv.length > 0) {
+      var $none = jQuery('.autoFix'),
+        autoDivHeight = $autoDiv.outerHeight(true),
+        autoDivBorder = autoDivHeight - $autoDiv.height(),
+        delBodyMargin = parseInt($body.css('marginTop')) + parseInt($body.css('marginBottom')),
+        none = $none.outerHeight(true) - $none.height(),
+        delHeight = 0;
+    
+      if ($autoDiv.data('rawHeight') === undefined) {
+        $autoDiv.data('rawHeight', $autoDiv.height());
+      }
+      var _rawHeight = $autoDiv.data('rawHeight');
+    
+      jQuery('.autoOff').each(function() {
+        delHeight += jQuery(this).outerHeight(true);
+      });
+    
+      var _height = $win.height() - delHeight - delBodyMargin - autoDivBorder - none;
+    
+      if (_height < _rawHeight) {
+        $autoDiv.height('auto');
+      } else {
+        $autoDiv.height(_height);
+      }
+    }
+  });
+	
+	// 主選單從中間上下打開
+  jQuery('.gMenu li').hover(function() {
+    
+    jQuery('.bg', this).css({left: '65px', width: 0, opacity: 0}).animate({left: 0, width: '130px', opacity: 1}, {queue: false, duration: 1200, easing: 'easeOutQuart'});
+  }, function() {
+    
+    jQuery('.bg', this).animate({left: '65px', width: 0, opacity: 0}, {queue: false, duration: 1200, easing: 'easeOutQuart'});
+  });
+
+});
---
+++ js/demo_website.js
@@ -0,0 +1,28 @@
+﻿jQuery(document).ready(function(e) {
+  /* 此為校稿頁面，非正式上線網站 */
+  if (/^demo|test\./.test(location.hostname)) {
+    var themeDir = (jQuery('link[href*="css/style."][href*=".css"]').attr('href') || '').replace(/css\/style.*.css(\?t=\d{0,})?/i, '');
+    var announce = function() {
+      setTimeout(function() {
+        jQuery.colorbox({
+          html: '<div id="grnetWebTest"><strong>提醒您!</strong><br>本站為校稿測試用途，非正式上線網站，特此聲明。</div>',
+          opacity: 0.6,
+          fixed: true,
+          innerHeight: 110
+        });
+      }, 1000);
+    };
+
+    jQuery('head').append('<link>').children('link:last').attr({rel: 'stylesheet', id: 'colorbox-pop-css', href: themeDir + 'css/demo_website.css'});
+
+    /* 判斷是否有載入 js */
+    if (jQuery('script[src*="js/plugins/jQuery/jquery.colorbox-1.4.15."][src*=".js"]').index() === -1) {
+      jQuery.ajaxSetup({cache: true});
+      jQuery.getScript(themeDir + 'js/plugins/jQuery/jquery.colorbox-1.4.15.min.js', function() {
+        announce();
+      });
+    } else {
+      announce();
+    }
+  }
+});
